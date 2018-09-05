e419a8f2603628108afd18a8fe5b3795a9e013c9
Jason Carter <jasonbradleycarter@gmail.com>
2018-09-05 10:53:37 +0000

    commit 1

---
+++ final-project/js/index.js
@@ -0,0 +1,31 @@
+/*
+//html
+header
+tabs/preset background
+  - <nav>, <h3>, <p>, <radio>
+form
+  - <form>, <input type="text">, <label>
+display
+  - <img in background>, <h1></h1>, <p>, <h3>, <div>
+
+
+//CSS
+display: flex/grid
+if grid, grid-template-columns: 1fr 1fr
+if flex in one container (width:50%)
+if each side has its own flex, wrap it
+if float, float: left
+input selection changes text box to blue
+
+
+//JS event
+event listener - keydown on ever keystroke
+transition btwn backgrounds
+    -transition when clicking
+    -actually change the background
+First initials in the display - slice() maybe?
+something like this: display.style.color = 'red'
+onClick new image loads for background
+fonts change when background changes
+
+*/
